6cf2c2f240e5e85d350f61b4fc788f702ae996d2
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 13:39:28 +0700

    Merubah tampilan product cart dan menambah button add

--- src/components/ProductCart.tsx
+++ src/components/ProductCart.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import React from "react";
 import {AiFillStar, AiOutlineStar} from "react-icons/ai";
-
+import {FaCartPlus} from "react-icons/fa";
 interface propsType {
   img: string;
   title: string;
@@ -58,29 +58,33 @@ const ProductCart: React.FC<propsType> = ({
   price,
 }) => {
   return (
-    <div className="px-4 border border-gray-200 rounded-xl mx-w-[400px]">
+    <div className="w-[250px] h-[350px] border border-sky-300 rounded-xl mx-w-[250px] p-4 flex flex-col justify-between transition-transform duration-300 hover:scale-105 shadow-md shadow-red-200">
       <div>
         <Image
-          className="w-full h-auto"
+          className="w-full h-[150px] object-cover rounded-md"
           src={img}
-          width={200}
-          height={300}
+          width={250}
+          height={150}
           alt={title}
         />
       </div>
 
-      <div className="space-y-2 py-2">
-        <h2 className="text-accent font-medium uppercase">{title}</h2>
-        <p className="text-gray-500 max-w-[150px]">{desc}</p>
+      <div className="space-y-2 py-2 flex-1">
+        <h2 className="text-sky-800 font-medium uppercase truncate">{title}</h2>
+        <p className="text-gray-500 text-sm line-clamp-2">{desc}</p>
         <div>{generateRating(rating)}</div>
-
-        <div className="font-bold flex gap-4">
+        <div className="font-bold flex gap-4 items-center">
           Rp. {price}
           <del className="text-gray-500 font-medium">
             Rp. {parseInt(price) + 100}.000
           </del>
         </div>
       </div>
+
+      <button className="bg-sky-900 text-white font-semibold py-2 rounded-t-lg mt-4 hover:bg-sky-800 transition-colors duration-300 flex items-center justify-center gap-2">
+        <FaCartPlus className="text-lg" />
+        Add to Cart
+      </button>
     </div>
   );
 };
