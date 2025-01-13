4fd37112b4de8b14857c0d3dbff639d2b7a5a8e7
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 13:47:20 +0700

    Merubah warna background color footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
+    <div className="bg-sky-950 text-gray-500 text-center py-4 pb-16 md:pb-4">
       Copyright @BillWatch <br /> All Rights Reserved 2024.
     </div>
   );
